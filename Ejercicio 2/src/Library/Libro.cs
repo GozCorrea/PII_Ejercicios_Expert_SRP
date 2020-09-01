27122ca2fe2e2ff989f4823315e772c86d1684f8
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-09-01 20:24:59 -0300

    add justification

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+/* No cumple Expert ya que esta clase posee mas informacion de la necesaria:
+     el libro no tiene porque conocer la estanteria
+   NO cumple SRP ya que tiene mas de una razon de cambio: 
+    Si se quisiera editar para incluir total de paginas de libro es una razon de cambio
+    Si se quisiera editar la forma de almacenamiento seria otra razon de cambio
 
+*/
 namespace SRP
 {
     public class Libro
