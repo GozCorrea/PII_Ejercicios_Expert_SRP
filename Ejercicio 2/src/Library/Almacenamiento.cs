69fbc4990bbbd8f74c272f1ed66f9bb823a850ff
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-09-01 20:42:01 -0300

    cambio clases

---
+++ Ejercicio 2/src/Library/Almacenamiento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SRP
+{
+    public class Almacenamiento
+    {
+        public string SectorBiblioteca { get ; set; }
+        public string EstanteBiblioteca { get ; set; }
+
+        public void AlmacenarLibro(String sector, String estante)
+        {
+            this.SectorBiblioteca = sector;
+            this.EstanteBiblioteca = estante;
+        }
+
+    }
+}
--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+/* No cumple Expert ya que esta clase posee mas informacion de la necesaria:
+     el libro no tiene porque conocer la estanteria
+   NO cumple SRP ya que tiene mas de una razon de cambio: 
+    Si se quisiera editar para incluir total de paginas de libro es una razon de cambio
+    Si se quisiera editar la forma de almacenamiento seria otra razon de cambio
+    
 
+*/
 namespace SRP
 {
     public class Libro
@@ -8,8 +15,6 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
 
         public Libro(String titulo, String autor, String codigo)
         {
@@ -18,11 +23,6 @@ namespace SRP
             this.Codigo = codigo;
         }
 
-        public void AlmacenarLibro(String sector, String estante)
-        {
-            this.SectorBiblioteca = sector;
-            this.EstanteBiblioteca = estante;
-        }
 
     }
 }
