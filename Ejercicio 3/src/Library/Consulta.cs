2d73b128ffa80861fc030f7972e5732ab75978ee
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-09-04 20:10:45 -0300

    semifinal

--- Ejercicio 3/src/Library/Consulta.cs
+++ Ejercicio 3/src/Library/Consulta.cs
@@ -5,6 +5,16 @@ namespace Library
 {
     public class Consulta
     {
-        
+        public static String CrearConsulta (Doctor a, Paciente b, Clinica clinica, String date, String id)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
+            Boolean isValid = true;
+            if (isValid)
+            {
+                stringBuilder.Append("Appoinment Scheduled");
+            }
+            return stringBuilder.ToString();
+
+        }
     }
 }
